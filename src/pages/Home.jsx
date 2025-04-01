04877faa04454e25ca7572d0461bd17299e6e432
“AntonioAmbrosecchia” <antonyambro96@gmail.com>
2025-04-01 13:31:58 +0200

    create home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <div>
+            <h1>test Home</h1>
+            <p>Questa è la homepage.</p>
+        </div>
+    );
+};
+
+export default Home;
