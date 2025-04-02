a86209bb016776e91599a63edebe455e2a40e81f
“AntonioAmbrosecchia” <antonyambro96@gmail.com>
2025-04-02 15:52:34 +0200

    update Post list

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const Posts = () => {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        fetch("https://67c5b4f3351c081993fb1ab6.mockapi.io/api/posts")
+            .then((response) => response.json())
+            .then((data) => setPosts(data.slice(0, 5)))  // Mostra solo i primi 5 post
+            .catch((error) => console.error("Errore nel recupero dei post:", error));
+    }, []);
+
     return (
         <div>
             <h1>Lista dei Post</h1>
-            <p>Post del blog</p>
+            <ul>
+                {posts.map((post) => (
+                    <li key={post.id}>
+                        <Link to={`/posts/${post.id}`}>{post.title}</Link>
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 };
